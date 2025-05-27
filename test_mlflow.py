3c96c93a9e8681accbf8a7c94bbe0e8b6a89e5c9
Luis Armando Lazarte <la.lazartep@alum.up.edu.pe>
2025-05-27 10:14:02 -0500

    primer cambio

--- test_mlflow.py
+++ test_mlflow.py
@@ -2,6 +2,7 @@ import mlflow
 from mlflow import log_metric, log_param, log_artifact, start_run
 
 mlflow.set_tracking_uri("http://localhost:5000")  # importante
+mlflow.set_experiment("mi_primer_experimento") 
 
 if __name__ == '__main__':
     with start_run():
